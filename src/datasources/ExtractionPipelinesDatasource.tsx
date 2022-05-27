17698de2deb98998c008bfd8e774a5394913a3bd
Zsolt Kiss <99021303+KissSpinobleZsolt@users.noreply.github.com>
2022-05-27 16:27:33 +0300

    added multi select and  get all

--- src/datasources/ExtractionPipelinesDatasource.tsx
+++ src/datasources/ExtractionPipelinesDatasource.tsx
@@ -5,17 +5,43 @@ import { Connector } from '../connector';
 
 interface ExtractionPipelinesResponse {
   status?: string | boolean;
-  createdTime?: TimeRange;
+  createdTime?: Date;
   message?: string;
   [x: string]: any;
 }
 // eslint-disable-next-line no-nested-ternary
 const evalStatus = (text) => (text === 'success' ? 1 : text === 'failure' ? 0 : 2);
+const convert = (arr) => {
+  return _.map(
+    arr,
+    ({
+      metadata,
+      createdTime,
+      lastSeen,
+      lastSuccess,
+      id,
+      dataSetId,
+      lastUpdatedTime,
+      lastFailure,
+      contacts,
+      ...rest
+    }) => ({
+      ...rest,
+      id: `${id}`,
+      dataSetId: `${dataSetId}`,
+      createdTime: new Date(createdTime),
+      lastSeen: new Date(lastSeen),
+      lastSuccess: new Date(lastSuccess),
+      lastUpdatedTime: new Date(lastUpdatedTime),
+      lastFailure: new Date(lastFailure),
+      ...contacts,
+      ...metadata,
+    })
+  );
+};
 export class ExtractionPipelinesDatasource {
   public constructor(private connector: Connector) {}
-
-  private runQuery(options) {
-    const { id, numeric } = options;
+  private postQuery(id, numeric) {
     return this.connector
       .fetchItems({
         path: '/extpipes/runs/list',
@@ -33,6 +59,10 @@ export class ExtractionPipelinesDatasource {
           : response
       );
   }
+  private runQuery(options) {
+    const { ids, numeric } = options;
+    return Promise.all(_.map(ids, ({ id }) => this.postQuery(id, numeric)));
+  }
   async query(options: DataQueryRequest<CogniteQuery>): Promise<DataQueryResponse> {
     const data = await Promise.all(
       options.targets.map((target) => {
@@ -42,8 +72,13 @@ export class ExtractionPipelinesDatasource {
         });
       })
     );
+    const all = _.filter(options.targets, {
+      extractionPipelinesQuery: { enableAllExtractionPipelines: true },
+    }).length
+      ? [convert(await this.getAllExtractionPipelines())]
+      : [];
     return {
-      data,
+      data: _.concat(all as ExtractionPipelinesResponse, ...data),
     };
   }
   async getAllExtractionPipelines() {
